e2d1c011ed146fd525cb5199cab9df43b36db9c8
Devin Rhode <devinrhode2@gmail.com>
2013-02-27 14:26:06 -0800

    call additionalFn with args array and prevFunc

--- extendFunction.js
+++ extendFunction.js
@@ -1,3 +1,4 @@
+//old history: https://gist.github.com/devinrhode2/5022364
 function extendFunction(fnRef, addedFunctionality) {
   var oldOldFn;
   if (Object.prototype.toString.call(fnRef) =='[object String]') {
@@ -11,15 +12,17 @@ function extendFunction(fnRef, addedFunctionality) {
   }
  
   var newFunc = function() {
-    var args = [].slice.call(arguments, 0);
+    var args = [].slice.call(arguments);
  
     var called = false;
     var oldFunction = function() {
       called = true;
       return oldOldFn.apply(this, [].slice.call(arguments));
+      //could use `args` instead of `arguments`, but then we assume you aren't changing the args
+      //and in fact, if you change the args you call oldFunction with, things won't work as expected
     };
  
-    var newRet = addedFunctionality.apply(this, args.concat(oldFunction));
+    var newRet = addedFunctionality.call(this, args, oldFunction);
     if (!called) {
       called = false; // reset in case a function dynamically calls the oldFunction
       var oldRet = oldFunction.apply(this, args);
@@ -32,7 +35,7 @@ function extendFunction(fnRef, addedFunctionality) {
     }
   };
  
-  if (s.length === 0) {
+  if (s && s.length === 0) {
     eval('window.' + fnRef + ' = ' + newFunc.toString());
   } else {
     return newFunc;
