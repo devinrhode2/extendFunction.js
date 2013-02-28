75dde3d038890cd408ae45e63ab8541062753749
Devin Rhode <devinrhode2@gmail.com>
2013-02-27 16:38:24 -0800

    Forgot what I was doing But I know I did something

--- extendFunction.js
+++ extendFunction.js
@@ -2,8 +2,8 @@
 function extendFunction(fnRef, addedFunctionality) {
   var oldOldFn;
   if (Object.prototype.toString.call(fnRef) =='[object String]') {
+    oldOldFn = window || global;
     var s = fnRef.split('.');
-    oldOldFn = window;
     while (s.length) {
       oldOldFn = oldOldFn[s.shift()];
     }
@@ -12,26 +12,34 @@ function extendFunction(fnRef, addedFunctionality) {
   }
  
   var newFunc = function() {
-    var args = [].slice.call(arguments);
- 
-    var called = false;
-    var oldFunction = function() {
-      called = true;
-      return oldOldFn.apply(this, [].slice.call(arguments));
-      //could use `args` instead of `arguments`, but then we assume you aren't changing the args
-      //and in fact, if you change the args you call oldFunction with, things won't work as expected
-    };
- 
-    var newRet = addedFunctionality.call(this, args, oldFunction);
-    if (!called) {
-      called = false; // reset in case a function dynamically calls the oldFunction
-      var oldRet = oldFunction.apply(this, args);
-    }
- 
-    if (typeof newRet === 'undefined') {
-      return oldRet;
-    } else {
-      return newRet;
+    try {
+      var args = [].slice.call(arguments);
+
+      var called = false;
+      var oldFunction = function() {
+        called = true;
+        return oldOldFn.apply(this, [].slice.call(arguments));
+        //could use `args` instead of `arguments`, but then we assume you aren't changing the args
+        //and in fact, if you change the args you call oldFunction with, things won't work as expected
+      };
+
+      var newRet = addedFunctionality.call(this, args, oldFunction);
+      if (!called) {
+        called = false; // reset in case a function dynamically calls the oldFunction
+        var oldRet = oldFunction.apply(this, args);
+      }
+
+      if (typeof newRet === 'undefined') {
+        return oldRet;
+      } else {
+        return newRet;
+      }
+    } catch (e) {
+      if (typeof onuncaughtError !== 'undefined') { //probably window.onuncaughtError but maybe not. you can var over it
+        onuncaughtError(e);
+      } else {
+        throw e;
+      }
     }
   };
  
