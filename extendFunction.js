3580e8a4fcd5fad8dcb3c4a81d71d86c0e13304f
Devin Rhode <devinrhode2@gmail.com>
2013-11-09 03:14:23 -0800

    taking break. idk if I really want to rename to userFn, but perhaps..

--- extendFunction.js
+++ extendFunction.js
@@ -14,6 +14,8 @@
     // however, if a global 'use strict' is leaked, you can expect we just use the `this`
     // keyword.. (I wish I could solve your bugs for you, but I can't here)
 
+    var userFn;
+
     // type check like underscore/lodash
     if (Object.prototype.toString.call(fnPropertyRef) == '[object String]') {
 
@@ -21,23 +23,23 @@
       var propertyArray = fnPropertyRef.split('.');
 
       // start with the global object, and iteratively access each property,
-      // re-assigning to oldFn each time
+      // re-assigning to userFn each time
       // For example, this dynamic code:
-      //  oldFn = window; oldFn = oldFn[prop]; oldFn = oldFn[nextProp]; ..
+      //  userFn = window; userFn = userFn[prop]; userFn = userFn[nextProp]; ..
       // Could ultimately boil down to this static code:
-      //  oldFn = window.$.fn.jQueryPluginFunction;
-      var oldFn = window;
+      //  userFn = window.$.fn.jQueryPluginFunction;
+      userFn = window;
 
       // so while there are properties left to access..
       while (propertyArray.length) {
         try {
-          oldFn = oldFn[propertyArray.shift()];
-          //  if  oldFn[propArray[0]]       is      undefined,
-          // then oldFn[propArray[0]][propArray[1]] will throw because this is essentially doing window.undefined.undefined
+          userFn = userFn[propertyArray.shift()];
+          //  if  userFn[propArray[0]]       is      undefined,
+          // then userFn[propArray[0]][propArray[1]] will throw because this is essentially doing window.undefined.undefined
         } catch (cantReadPropOfUndefined) {
-          // And now we've caught that exception. oldFn should still just === undefined (essentially like window.undefined aka oldFn.notDefinedProperty)
-          if (oldFn === undefined) {
-            // ...but don't we want to prevent having oldFn.undefined in the first place?
+          // And now we've caught that exception. userFn should still just === undefined (essentially like window.undefined aka userFn.notDefinedProperty)
+          if (userFn === undefined) {
+            // ...but don't we want to prevent having userFn.undefined in the first place?
             // Nope, we just assume good input for efficiency, but catch the exception here when it happens.
             throw new TypeError('window.' + fnPropertyRef + ' is undefined and therefore cannot be extended as a function.');
           } else {
@@ -47,55 +49,51 @@
         }
       }
     } else {
-      // else fnPropertyRef is actually the oldFn, or at least we'll assume so and catch the error if it isn't
-      oldFn = fnPropertyRef;
+      // else fnPropertyRef is actually the userFn, or at least we'll assume so and catch the error if it isn't
+      userFn = fnPropertyRef;
     }
 
-    //oldFn should now be a function. (If it isn't we'll catch that error specifically)
+    //userFn should now be a function. (If it isn't we'll catch that error specifically)
 
     function extendedFunction() {
       var args = Array.prototype.slice.call(arguments); // we use Array.prototype.slice instead of [].slice because it doesn't allocate a new array
 
-      // extend oldFn to track if it was called
+      // extend userFn to track if it was called
       var called = false;
-      var orig_oldFn = oldFn;
-      oldFn = function () {
+      var orig_userFn = userFn;
+      userFn = function () {
         called = true;
         try {
           // should we store this above and then use that variable? I don't know
-          return orig_oldFn.apply(this, Array.prototype.slice.call(arguments));
+          return orig_userFn.apply(this, Array.prototype.slice.call(arguments));
           // we use standard dynamic `arguments` instead of `args` because there are not necessarily always the same
           // if a user modifies the arguments they call originalFunction with (extendFunction(function(args, originalFunction){ .. ) then we have to respect that
         } catch (e) {
-          // above we assumed oldFn is a function if it's not a string (for efficiency) - here, we catch and correct if it wasn't a function.
+          // above we assumed userFn is a function if it's not a string (for efficiency) - here, we catch and correct if it wasn't a function.
           // yes, it's more efficient to originally assume it's a function
-          if (Object.prototype.toString.call(orig_oldFn) != '[object Function]') {
+          if (Object.prototype.toString.call(orig_userFn) != '[object Function]') {
             throw new TypeError(fnPropertyRef + ' is not a function. ' + fnPropertyRef + ' toString value is:' +
-                            orig_oldFn + ' and is of type:' + typeof orig_oldFn);
+                            orig_userFn + ' and is of type:' + typeof orig_userFn);
           } else {
-            if (window.onuncaughtException) {
-              onuncaughtException(e);
-            } else {
-              throw e
-            }
+            fireUncaughtExcepton(e);
           }
         }
       };
 
       var modifiedThis = this;
-      var callOldFn = true;
-      function iWillCallOldFn() {
-        callOldFn = false; //don't call oldFn
+      var calluserFn = true;
+      function iWillCalluserFn() {
+        calluserFn = false; //don't call userFn
       }
       // PROPOSAL: api for telling extendFunction not to call the oldFunction itself,
       // because the users additionalFunctionality will call it asynchronously
-      modifiedThis.iWillCallOldFn = iWillCallOldFn;
+      modifiedThis.iWillCalluserFn = iWillCalluserFn;
       var oldRet;
-      var newRet = addedFunctionality.call(modifiedThis, args, oldFn);
+      var newRet = addedFunctionality.call(modifiedThis, args, userFn);
       if (!called) {
-        called = false; // reset in case a function dynamically calls the oldFn
-        if (callOldFn) {
-          oldRet = oldFn.apply(this, args);
+        called = false; // reset in case a function dynamically calls the userFn
+        if (calluserFn) {
+          oldRet = userFn.apply(this, args);
         }
       }
 
@@ -106,14 +104,21 @@
       }
     }
 
-    //preserve function.length since extendedFunction doesn't list arguments!
-    extendedFunction.length = oldFn.length;
-    //maintain prototype chain..
-    extendedFunction.prototype = oldFn.prototype;
+    // Preserve function.length since extendedFunction doesn't list arguments!
+    extendedFunction.length = userFn.length;
+    // Maintain prototype chain..
+    extendedFunction.prototype = userFn.prototype;
     //TODO: I'm not sure if someone does `new extendedFunction(..)` nothing different will happen
 
     // Maintain constructor property
-    extendedFunction.constructor = oldFn.constructor;
+    extendedFunction.constructor = userFn.constructor;
+
+    // Ensure all properties are copied over..
+    for (var prop in userFn) {
+      if (Object.prototype.hasOwnProperty.call(userFn, prop)) {
+        extendedFunction[prop] = userFn[prop];
+      }
+    }
 
     if (propertyArray && propertyArray.length === 0) {
       eval('(typeof window !== "undefined" ? window : global).' + fnPropertyRef + ' = ' + extendedFunction.toString());
